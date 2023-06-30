a2d3bbed7179d315aeeb75fe752d419235918cbe
mzk468 <mzkhaled468@gmail.com>
2023-06-30 19:13:42 +0100

    Insanity (working on ittt)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 const app = new Vue({
   el: '#app',
   data: {
-    appName: 'Avatar Creator'
+    appName: 'Avatar Creator',
+
+    currentBase: 0,
+    baseLayers: [
+      './img/base/0.png'
+    ],
+
+    currentBody: 0,
+    bodyLayers: [
+      './img/body/0.png'
+    ],
+
+    currentFace: 0,
+    faceLayers: [
+
+    ],
+
+    currentMouth: 0,
+    mouthLayers: [
+
+    ]
   },
   computed: {},
   methods: {}
