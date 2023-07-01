f1cf4ee1028f85e2c1e1a5efb416a521d518ed6e
mzk468 <mzkhaled468@gmail.com>
2023-07-01 11:09:55 +0100

    Refactoring and introduction of avatar object

--- js/app.js
+++ js/app.js
@@ -3,25 +3,29 @@ const app = new Vue({
   data: {
     appName: 'Avatar Creator',
 
-    currentBase: 0,
+    avatar: {
+      name: '',
+      base: 0,
+      body: 0,
+      face: 0,
+      hair: 0
+    },
+
     baseLayers: [
       './img/base/0.png',
       './img/base/1.png'
     ],
 
-    currentBody: 0,
     bodyLayers: [
       './img/body/0.png',
       './img/body/1.png'
     ],
 
-    currentFace: 0,
     faceLayers: [
       './img/face/0.png',
       './img/face/1.png'
     ],
 
-    currentHair: 0,
     hairLayers: [
       './img/hair/0.png',
       './img/hair/1.png'
@@ -33,66 +37,66 @@ const app = new Vue({
     // Base methods
 
     baseRight() {
-      if (this.currentBase !== this.baseLayers.length - 1) // i.e. pointer within array bounds
-        this.currentBase++;
+      if (this.avatar.base !== this.baseLayers.length - 1) // i.e. pointer within array bounds
+        this.avatar.base++;
       else
-        this.currentBase = 0; // place on other end
+        this.avatar.base = 0; // place on other end
     },
 
     baseLeft() {
-      if (this.currentBase !== 0) // i.e. pointer within array bounds
-        this.currentBase--;
+      if (this.avatar.base !== 0) // i.e. pointer within array bounds
+        this.avatar.base--;
       else
-        this.currentBase = this.baseLayers.length - 1; // place on other end
+        this.avatar.base = this.baseLayers.length - 1; // place on other end
     },
 
     // Body methods
 
     bodyRight() {
-      if (this.currentBody !== this.bodyLayers.length - 1) // i.e. pointer within array bounds
-        this.currentBody++;
+      if (this.avatar.body !== this.bodyLayers.length - 1) // i.e. pointer within array bounds
+        this.avatar.body++;
       else
-        this.currentBody = 0; // place on other end
+        this.avatar.body = 0; // place on other end
     },
 
     bodyLeft() {
-      if (this.currentBody !== 0) // i.e. pointer within array bounds
-        this.currentBody--;
+      if (this.avatar.body !== 0) // i.e. pointer within array bounds
+        this.avatar.body--;
       else
-        this.currentBody = this.bodyLayers.length - 1; // place on other end
+        this.avatar.body = this.bodyLayers.length - 1; // place on other end
     },
 
     // Face methods
 
 
     faceRight() {
-      if (this.currentFace !== this.faceLayers.length - 1) // i.e. pointer within array bounds
-        this.currentFace++;
+      if (this.avatar.face !== this.faceLayers.length - 1) // i.e. pointer within array bounds
+        this.avatar.face++;
       else
-        this.currentFace = 0; // place on other end
+        this.avatar.face = 0; // place on other end
     },
 
     faceLeft() {
-      if (this.currentFace !== 0) // i.e. pointer within array bounds
-        this.currentFace--;
+      if (this.avatar.face !== 0) // i.e. pointer within array bounds
+        this.avatar.face--;
       else
-        this.currentFace = this.faceLayers.length - 1; // place on other end
+        this.avatar.face = this.faceLayers.length - 1; // place on other end
     },
 
     // Hair methods
 
     hairRight() {
-      if (this.currentHair !== this.hairLayers.length - 1) // i.e. pointer within array bounds
-        this.currentHair++;
+      if (this.avatar.hair !== this.hairLayers.length - 1) // i.e. pointer within array bounds
+        this.avatar.hair++;
       else
-        this.currentHair = 0; // place on other end
+        this.avatar.hair = 0; // place on other end
     },
 
     hairLeft() {
-      if (this.currentHair !== 0) // i.e. pointer within array bounds
-        this.currentHair--;
+      if (this.avatar.hair !== 0) // i.e. pointer within array bounds
+        this.avatar.hair--;
       else
-        this.currentHair = this.hairLayers.length - 1; // place on other end
+        this.avatar.hair = this.hairLayers.length - 1; // place on other end
     }
   }
 });
