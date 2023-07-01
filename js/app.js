ea305e2c53800d8ac34a1bfdb71f1a29d0d6221d
mzk468 <mzkhaled468@gmail.com>
2023-07-01 12:46:49 +0100

    Deleted test assets, code ready for new assets

--- js/app.js
+++ js/app.js
@@ -13,22 +13,54 @@ const app = new Vue({
 
     baseLayers: [
       './img/base/0.png',
-      './img/base/1.png'
+      './img/base/1.png',
+      './img/base/2.png',
+      './img/base/3.png',
+      './img/base/4.png',
+      './img/base/5.png',
+      './img/base/6.png',
+      './img/base/7.png',
+      './img/base/8.png',
+      './img/base/9.png'
     ],
 
     bodyLayers: [
       './img/body/0.png',
-      './img/body/1.png'
+      './img/body/1.png',
+      './img/body/2.png',
+      './img/body/3.png',
+      './img/body/4.png',
+      './img/body/5.png',
+      './img/body/6.png',
+      './img/body/7.png',
+      './img/body/8.png',
+      './img/body/9.png'
     ],
 
     faceLayers: [
       './img/face/0.png',
-      './img/face/1.png'
+      './img/face/1.png',
+      './img/face/2.png',
+      './img/face/3.png',
+      './img/face/4.png',
+      './img/face/5.png',
+      './img/face/6.png',
+      './img/face/7.png',
+      './img/face/8.png',
+      './img/face/9.png'
     ],
 
     hairLayers: [
       './img/hair/0.png',
-      './img/hair/1.png'
+      './img/hair/1.png',
+      './img/hair/2.png',
+      './img/hair/3.png',
+      './img/hair/4.png',
+      './img/hair/5.png',
+      './img/hair/6.png',
+      './img/hair/7.png',
+      './img/hair/8.png',
+      './img/hair/9.png'
     ]
   },
   computed: {},
